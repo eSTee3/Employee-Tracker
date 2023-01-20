50ced5de4d38402ceb225648fd887137888bff4f
eSTee3 <tagsxa@yahoo.com>
2023-01-20 16:34:38 -0600

    Added files, populated schema and added them into folders

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+-- Department-based table schema
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Role-based table schema
+CREATE TABLE role(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (11,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+-- Employee-based table schema
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    -- Allows manager_id to be null, if an employee does not have a manager
+    manager_id INT,
+)
