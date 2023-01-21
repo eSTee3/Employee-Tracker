41427153a1cf0a680dbf363a174e1cd7601f13c3
eSTee3 <tagsxa@yahoo.com>
2023-01-20 18:32:36 -0600

    Cleaned up coding structure a bit

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,25 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments(
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-name VARCHAR(30) NOT NULL);
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles(
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(11,2) NOT NULL,
-department_id INT NOT NULL,
-FOREIGN KEY (department_id) REFERENCES departments(id));
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(11,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
 
 CREATE TABLE employees(
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT,
-FOREIGN KEY (role_id) REFERENCES roles(id),
-FOREIGN KEY (manager_id) REFERENCES employees(id));
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    -- Allows for manager_id to be null (for employees that are also managers)
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
